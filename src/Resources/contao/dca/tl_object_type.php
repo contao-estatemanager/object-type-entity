4ea78950dfe9f29013655a6ac80928b34fced710
Sebastian Zoglowek <sebastian@oveleon.de>
2022-11-03 18:25:56 +0100

    [Bugfix] Fix undefined array keys

--- src/Resources/contao/dca/tl_object_type.php
+++ src/Resources/contao/dca/tl_object_type.php
@@ -64,7 +64,7 @@ $GLOBALS['TL_DCA']['tl_object_type'] = array
             (
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
             (
